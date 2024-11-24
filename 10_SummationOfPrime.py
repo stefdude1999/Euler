8b9410b7aa1b666f44d8397ba64f04dafec8ba41
Stefan Caloian <stefc12341@outlook.com>
2024-11-23 23:53:45 -0500

    added summation of primes

---
+++ 10_SummationOfPrime.py
@@ -0,0 +1,17 @@
+import itertools
+
+def prime_sieve(end: int) -> list[int]:
+    sieve = [True] * end
+    sieve[0] = False
+    sieve[1] = False
+    for i in range(end):
+        if sieve[i]:
+            for factor in itertools.count(2):
+                number = factor * i
+                if number >= len(sieve):
+                    break
+                sieve[number] = False
+    primes = [number for number, state in enumerate(sieve) if state]
+    return primes
+
+print(sum(prime_sieve(2000000)))
